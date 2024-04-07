9b61481b614bb95099987e3c3733b51c4d5fd313
KunikaGithub <kunikarajput8@gmail.com>
2024-04-07 18:02:27 +0530

    My project

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -21,19 +21,34 @@ export const todoSlice = createSlice({
             state.todos=state.todos.filter((todo)=>
             todo.id!==action.payload)
         },
+<<<<<<< HEAD
         updateTodo: (state, action) => {
+=======
+         updateTodo: (state, action) => {
+>>>>>>> f16ccd0987b82ce8338a70aee1b8b1c8baca07c5
             const { id, newText } = action.payload;
             const todoIndex = state.todos.findIndex(todo => todo.id === id);
             if (todoIndex !== -1) {
                 state.todos[todoIndex].text = newText;
             }
+<<<<<<< HEAD
         }
+=======
+        },
+        
+>>>>>>> f16ccd0987b82ce8338a70aee1b8b1c8baca07c5
     }
 
 })
 
+<<<<<<< HEAD
 export const {addTodo , removeTodo , updateTodo} =todoSlice.actions
 
 export default todoSlice.reducer
 
 
+=======
+export const {addTodo, removeTodo , updateTodo} = todoSlice.actions
+
+export default todoSlice.reducer
+>>>>>>> f16ccd0987b82ce8338a70aee1b8b1c8baca07c5
